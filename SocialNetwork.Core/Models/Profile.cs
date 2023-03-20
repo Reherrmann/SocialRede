e4e31ad7eb22131c8cea5b4f952d1f15299931a6
Reherrmann <94018742+Reherrmann@users.noreply.github.com>
2023-03-19 21:48:09 -0300

    Add files via upload

---
+++ SocialNetwork.Core/Models/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Core.Models
+{
+    public class Profile
+    {
+        public int id {  get; set; }
+        public string FistName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PictureUrl { get; set; }
+
+        public string AccountId { get; set; }
+    }
+}
---
+++ SocialNetwork.Data/Repositories/ProfileRepository.cs
@@ -0,0 +1,27 @@
+﻿using SocialNetwork.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Data.Repositories
+{
+    public class ProfileRepository
+    {
+        private static IDictionary<string,Profile> _profiles;
+
+        public ProfileRepository() 
+        {
+            _profiles = new Dictionary<string, Profile>(); 
+        }
+
+        public void Create(Profile profile)
+        {
+            if (!_profiles.ContainsKey(profile.AccountId))
+            {
+                _profiles.Add(profile.AccountId, profile);
+            }
+        }
+    }
+}
